65c78192693d91003fe5d64444671b4461185c52
Chris T <gotaques@yahoo.com>
2019-05-31 11:50:10 -0700

    added LoanSubmit model

---
+++ src/app/_models/LoanSubmit.ts
@@ -0,0 +1,39 @@
+/*
+        //Loan Details
+        private string _clientName;
+        private string _projectName;
+        private string _type;
+        private string _loanDate;
+        private string _loanCondition;
+        private string _loanSupervisor;
+        private string _brand;
+        private string _modelNumber;
+        private string _serialNumber;
+        private string _assetNumber;
+        private string _note;
+        //Employee Details
+        private string _name;
+        private string _workAddress;
+        private string _homeAddress;
+        private long _phone;
+        private string _email;
+*/
+export class LoanSubmit {
+    ClientName: string;
+    ProjectName: string;
+    Type: string;
+    LoanDate: string;
+    LoanCondition: string;
+    LoanSupervisor: string;
+    Brand: string;
+    ModelNumber: string;
+    SerialNumber: string;
+    AssetNumber: string;
+    Note: string;
+
+    Name: string;
+    WorkAddress: string;
+    HomeAddress: string;
+    Phone: bigint;
+    Email: string;
+  }
