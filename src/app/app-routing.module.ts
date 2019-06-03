98452f788a071bf90c30ddd0e4922edb9b15c857
lukewreagan <lukewreagan@gmail.com>
2019-06-03 11:01:08 -0700

    added routing for loan-info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { InfoComponent } from './info/info.component';
 import { LoanFormComponent } from './loan-form/loan-form.component';
+import { LoanInfoComponent } from './loan-info/loan-info.component';
 
 const routes: Routes = [
   { path: '', component: UserComponent },
   { path: 'info', component: InfoComponent },
-  { path: 'loan', component: LoanFormComponent }
+  { path: 'loan', component: LoanFormComponent },
+  { path: 'loan-info', component: LoanInfoComponent },
 ];
 
 @NgModule({
