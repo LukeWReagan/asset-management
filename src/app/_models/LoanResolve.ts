ec7da50c3db7e57a3e356ca09d14d2f6df8b8074
Chris T <gotaques@yahoo.com>
2019-06-05 12:48:34 -0700

    added LoanResolve model

---
+++ src/app/_models/LoanResolve.ts
@@ -0,0 +1,12 @@
+/*
+private long _loanID;
+private string _returnDate;
+private string _returnSupervisor;
+private string _returnCondition;
+*/
+export class LoanResolve {
+    LoanID: bigint;
+    ReturnDate: string;
+    ReturnSupervisor: string;
+    ReturnCondition: string;
+}
